fbb679efa34ffaa7ab781bce1d40b1d56b054c08
mashar95 <67517489+mashar95@users.noreply.github.com>
2021-08-26 22:07:03 +0300

    Add files via upload

---
+++ New project/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1630004557,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ground-sheet0.png",
+		"images/phyblocks-sheet0.png",
+		"images/background-sheet0.png",
+		"images/background-sheet1.png",
+		"images/background-sheet2.png",
+		"images/background-sheet3.png",
+		"images/background-sheet4.png",
+		"images/background-sheet5.png",
+		"images/camera-sheet0.png",
+		"images/rotateblock-sheet0.png",
+		"images/buttons-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
