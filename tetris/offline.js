36cca01eef3eca65106e0caaacddbe68c73ce82f
mashar95 <67517489+mashar95@users.noreply.github.com>
2021-09-24 17:36:30 +0300

    Add files via upload

---
+++ tetris/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1632493883,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ground-sheet0.png",
+		"images/phyblocks-sheet0.png",
+		"images/background-sheet0.png",
+		"images/background-sheet1.png",
+		"images/background-sheet2.png",
+		"images/background-sheet3.png",
+		"images/background-sheet4.png",
+		"images/background-sheet5.png",
+		"images/buttons-sheet0.png",
+		"images/shadow-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/joy-sheet0.png",
+		"images/hpbar-sheet0.png",
+		"images/rot-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
